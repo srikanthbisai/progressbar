48d1b307149f94020ef1da8d609c5fe360f4757f
Srikanth Bisai <94598179+srikanthbisai@users.noreply.github.com>
2024-10-25 03:56:13 +0530

    Update ProgressBar.jsx

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -10,7 +10,7 @@ function ProgressBar({value = 0}) {
   return (
     <div className="relative font-serif flex flex-col items-center justify-center h-[20%] w-[500px] bg-gray-200 
     border border-neutral-400 rounded-full overflow-hidden">
-      <span className="z-10" style={{text: percent > 49 ? "white" : "black"}}>{percent.toFixed()} %</span>
+      <span className="z-10" style={{color: percent > 49 ? "white" : "black"}}>{percent.toFixed()} %</span>
       <div role="progressbar" 
       aria-valuemin={0}
       aria-valuemax={100}
